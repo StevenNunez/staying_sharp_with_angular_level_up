493ae893660a6e3ab88fa8c4cb567d6fd8ce540f
Steven Nunez <steven.nunez@gmail.com>
2015-09-08 14:04:21 -0400

    Level 2

--- public/js/directives/notes.js
+++ public/js/directives/notes.js
@@ -1,11 +1,23 @@
 (function() {
   var app = angular.module('notes-directives', []);
   app.directive('fisNote', function(){
+    var num = 1;
     return {
       restrict: "E",
+      scope: {
+        note: "="
+      },
       templateUrl: '/templates/pages/notes/fis-note.html',
       controller: function($scope){
-        $scope.title = "Note Title"
+        $scope.title = `Note Title ${num++}`;
+      },
+      link: function(scope, element, attrs){
+        var self = element;
+        scope.show = false;
+        element.find('a').on('click', function(e){
+          console.log 
+          e.preventDefault();
+        })
       }
     }
   })
